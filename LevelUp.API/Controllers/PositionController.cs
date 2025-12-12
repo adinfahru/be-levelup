c0c40ca8dcffb626ac460eaff3937ad5cfa539b1
adinfahru <mohamad.fahrudin18@gmail.com>
2025-12-12 20:52:59 +0700

    fix(PositionController): correct namespace import for utilities

--- LevelUp.API/Controllers/PositionController.cs
+++ LevelUp.API/Controllers/PositionController.cs
@@ -1,7 +1,7 @@
 using LevelUp.API.DTOs.Positions;
 using LevelUpAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using SkillSyncAPI.Utilities;
+using LevelUp.API.Utilities;
 
 namespace LevelUp.API.Controllers;
 
